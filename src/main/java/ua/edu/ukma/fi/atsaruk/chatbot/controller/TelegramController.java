16b1be5659b465e1025df0f7c5a3360631554451
Anatolii Tsaruk <atsaruk@codeminders.com>
2019-05-01 18:31:21 +0300

    watson assistant integration added

--- src/main/java/ua/edu/ukma/fi/atsaruk/chatbot/controller/TelegramController.java
+++ src/main/java/ua/edu/ukma/fi/atsaruk/chatbot/controller/TelegramController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ua.edu.ukma.fi.atsaruk.chatbot.service.TelegramService;
+import ua.edu.ukma.fi.atsaruk.chatbot.service.WatsonService;
 
 @RestController
 @RequestMapping(value = "telegram")
@@ -18,9 +19,13 @@ public class TelegramController {
   private static final Logger LOGGER = LoggerFactory.getLogger(TelegramController.class);
 
   private final TelegramService telegramService;
+  private final WatsonService watsonService;
 
-  public TelegramController(TelegramService telegramService) {
+  public TelegramController(
+      final TelegramService telegramService,
+      final WatsonService watsonService) {
     this.telegramService = telegramService;
+    this.watsonService = watsonService;
   }
 
   @PostMapping(value = "/{chatbotToken}")
@@ -28,9 +33,11 @@ public class TelegramController {
       @PathVariable final String chatbotToken,
       @RequestBody final Update data) {
     LOGGER.debug("POST telegram update received -> {}", data);
-    telegramService
-        .sendMessage(chatbotToken, data.getMessage().getChatId(), data.getMessage().getMessageId(),
-            data.getMessage().getText());
+
+    final long chatId = data.getMessage().getChatId();
+    final String text = data.getMessage().getText();
+    final String reply = watsonService.sendMessage(chatId, text);
+    telegramService.sendMessage(chatbotToken, chatId, reply);
 
     return ResponseEntity.ok(Boolean.TRUE);
   }
---
+++ src/main/java/ua/edu/ukma/fi/atsaruk/chatbot/service/WatsonService.java
@@ -0,0 +1,64 @@
+package ua.edu.ukma.fi.atsaruk.chatbot.service;
+
+import com.ibm.cloud.sdk.core.service.security.IamOptions;
+import com.ibm.watson.assistant.v1.Assistant;
+import com.ibm.watson.assistant.v1.model.Context;
+import com.ibm.watson.assistant.v1.model.MessageInput;
+import com.ibm.watson.assistant.v1.model.MessageOptions;
+import com.ibm.watson.assistant.v1.model.MessageResponse;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WatsonService {
+
+  private final Assistant assistant;
+  private final String workspaceId;
+  //TODO Replace with cache manager in the future
+  private final Map<Long, Context> contextCache;
+
+  public WatsonService(
+      @Value("${chatbot.watson.apiKey}") final String apiKey,
+      @Value("${chatbot.watson.version}") final String version,
+      @Value("${chatbot.watson.endpoint}") final String endpoint,
+      @Value("${chatbot.watson.workspaceId}") final String workspaceId,
+      final Map<Long, Context> contextCache) {
+
+    IamOptions options = new IamOptions.Builder()
+        .apiKey(apiKey)
+        .build();
+
+    this.assistant = new Assistant(version, options);
+    this.assistant.setEndPoint(endpoint);
+    this.assistant.setApiKey(apiKey);
+
+    this.workspaceId = workspaceId;
+    this.contextCache = contextCache;
+  }
+
+  public String sendMessage(long chatId, String message) {
+    final Context context;
+    if (contextCache.containsKey(chatId)) {
+      context = contextCache.get(chatId);
+    } else {
+      context = new Context();
+    }
+
+    MessageInput input = new MessageInput();
+    input.setText(message);
+
+    MessageOptions messageOptions = new MessageOptions
+        .Builder(workspaceId)
+        .input(input)
+        .context(context)
+        .build();
+
+    MessageResponse messageResponse = assistant.message(messageOptions).execute().getResult();
+    contextCache.put(chatId, messageResponse.getContext());
+
+    return messageResponse.getOutput().getText().stream()
+        .reduce((a, b) -> a + System.lineSeparator() + b)
+        .orElse("");
+  }
+}
